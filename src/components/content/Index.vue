fbf912d3260fc46af3ced9d042670d24382725e5
liyanan666 <775055297@qq.com>
2017-05-16 19:17:30 +0800

    Update Index.vue

--- src/components/content/Index.vue
+++ src/components/content/Index.vue
@@ -1,10 +1,21 @@
 <template>
 	<div id="">
 		<ul class="foodlist">
-			<li v-for="(item,index) in goods" :key="index" @click="getindex(index)" :class="index=menuindex?active:''">
-				{{item.name}} {{index}}
+			<li v-for="(val,index) in goods" @click="getindex(index)" :class="index==menuindex?'active':''">
+				{{val.name}}
 			</li>
 		</ul>
+		<div class="foodinfo">
+			<ul>
+				<li v-for="(val1,index) in goods">
+					<h2 class="title"><span></span>{{val1.name}}</h2> 
+					<!--<li v-for="(val2,index) in val1.foods">
+						{{val2.name}}
+					</li>-->
+					<h1 v-for="(val,index) in val1.foods">{{val.name}}</h1>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -12,33 +23,54 @@
 	export default {
 		data(){
 			return{
+				food:{},
 				menuindex:0
 			}
 		},
+		
 		props:['goods'],
+		created() {
+			this.food = this.goods
+		},
 		methods:{
 			getindex(index){
-				alert(index)
 				this.menuindex=index
 			}
 		}
-	}
+	}       
 </script>
 
 <style>
 .foodlist{
-	width: 110px;
+	display: inline-block;
+	width: 29%;
 	background: #f3f5f7;
 }
 .foodlist li{
 	height: 50px;
 	line-height: 50px;
-	width: 100px;
-	margin-left: 5px;
 	text-align: center;
 	border-bottom:1px solid #e2e2e2;
 }
 .foodlist .active{
 	background: #FFFFFF;
+}
+.foodinfo{
+	width: 69%;
+	display: inline-block;
+	vertical-align: top;
+}
+.title{
+	height: 30px;
+	line-height: 30px;
+	background: #E2E2E2;
+}
+.title span{
+	width: 2px;
+	height: 30px;
+	background: gray;
+	float: left;
+	margin-left: -2px;
+	display: inline-block;
 }
 </style>
